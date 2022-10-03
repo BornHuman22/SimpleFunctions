bf137a6fcfb15ad49261214adfa5c584c36e41b3
BornHuman22 <james.etchison2011@yahoo.com>
2022-10-03 11:57:15 -0400

    created the first exercise wb3 pg10

---
+++ scripts/simple_function.js
@@ -0,0 +1,18 @@
+function favoriteThings(){
+    console.log("Name: James");
+    console.log("Movie: New Jack City");
+    console.log("Musician/Band: Pre-Fed Gucci Mane")
+}
+
+function whyImHere(){
+    console.log("I am here to grow in the technical field and get a job that does not require physical labor");
+}
+
+function favoritePlace(){
+    console.log("My favorite place is my house")
+}
+
+//*call the function out here*//
+favoriteThings();
+whyImHere();
+favoritePlace();
